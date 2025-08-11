3427535caac99d7d33295ca8ede9f5f95b85d976
Amir Mehrizi <amir.mehrizi25@gmail.com>
2025-08-11 16:27:48 +0330

    finish demo code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,49 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ChatService } from './chat.service';
+import { Subject, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   isOpen = false;
+  messages = signal<string[]>([]);
+  newMessage = signal('');
+  private destroy$ = new Subject<void>();
+  user = { name: 'John', status: 'online' };
+  constructor(private chat: ChatService) {
+  }
+  ngOnInit() {
+    this.chat.startMock();
 
-
-
-
-  toggle(){
+    this.chat.messages$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(msg =>
+        this.messages.update(list => [...list, msg])
+      );
+  }
+  toggle() {
     this.isOpen = !this.isOpen;
+  }
+  send() {
+    const text = this.newMessage().trim();
+    if (!text) return;
+    this.chat.send(text);
+    this.newMessage.set('');
+  }
+
+  toggleStatus() {
+    this.user.status = this.user.status === 'online' ? 'offline' : 'online';
+  }
+
+  ngOnDestroy(): void {
+
   }
 }
